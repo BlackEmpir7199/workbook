8accac83307117023fa1fba92725d961c21ba81a
BlackEmpir7199 <blackempire9177@gmail.com>
2025-10-09 23:34:05 +0530

    added a badtz ui bg and button and wrapped it with clerk login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,30 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { HyperspaceBackground } from "@/components/ui/hyperspace-background";
+import { StarButton } from "./components/ui/star-button";
 
 export default function App() {
   return (
-    <header>
+    <div className="relative flex h-[100%] w-full items-center justify-center overflow-hidden bg-black flex-col">
+      <HyperspaceBackground />
+      <h1 className="font-poppins relative z-10 bg-gradient-to-br from-white to-zinc-400 bg-clip-text px-2 py-2 text-center text-3xl  lg:text-6xl font-semibold text-transparent">
+        Unlock Your WorkBook <br />
+        Where Productivy happens
+      </h1>
       <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+          <SignInButton mode="modal">
+          <StarButton className="rounded-3xl" >Sing In</StarButton>
+          </SignInButton >
+        </SignedOut>
+    </div>
+      /* <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header> */
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,14 @@ import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk Publishable Key to the .env file");
-}
-
-  createRoot(document.getElementById("root")).render(
-    <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {PUBLISHABLE_KEY ? (
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
       </ClerkProvider>
-    </StrictMode>
-  );
+    ) : (
+      <App />
+    )}
+  </StrictMode>
+);
